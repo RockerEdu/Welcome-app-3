6fef886365ca983823a1564c58e3faf2d981ef26
RockerEdu <rocker_edu@hotmail.com>
2025-02-06 23:51:19 -0600

    Add files via upload

---
+++ welcome-3/welcome-3/welcome_3App.swift
@@ -0,0 +1,17 @@
+//
+//  welcome_3App.swift
+//  welcome-3
+//
+//  Created by Eduardo Navarro on 06/02/25.
+//
+
+import SwiftUI
+
+@main
+struct welcome_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
